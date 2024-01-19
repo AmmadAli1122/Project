b4469df89613c22aaebdf7efe7718d53d239303d
AmmadAli1122 <155666572+AmmadAli1122@users.noreply.github.com>
2024-01-19 23:04:25 +0500

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,26 @@
+def C_To_F():
+    c=float(input("Input temperature in celsius:"))
+    F=((c*1.8)+32)
+    print("Temperature in fahrenheit is","%.2f"%F,"°F")
+
+
+
+def F_To_C():
+    f=float(input("Input temperature in fahrenheit:"))
+    C=((f-32)/1.8)
+    print("Temperature in celsius is","%.2f"%F,"°C")
+
+def main():
+    print('''
+             A.Convert Temperature Celsius To Fahrenheit
+             B.Convert Temperature Celsius To Fahrenheit''')
+    X=input("Enter Your Choice..?")
+    if (X=='A'):
+        C_To_F()
+
+    elif (X=='B'):
+        F_To_C()
+
+    else:
+        print("Wrong Choice..!")
+main()
